b332b196ccaf0cc68f3b921010ab3e05d55281ed
sidecity.li <sidecity.li@osl.com>
2024-11-29 19:39:32 +0800

    feat: 打包时去掉hot代码

--- packages/exposure-burying/vite.config.ts
+++ packages/exposure-burying/vite.config.ts
@@ -2,29 +2,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const entry = path.resolve(__dirname, "./src/index.tsx");
+
 // https://vite.dev/config/
 
-export default defineConfig(() => {
-  const entry = path.resolve(__dirname, "./src/index.tsx");
-  return {
-    mode: "production",
-    build: {
-      // 如下是默认值
-      // target: "modules",
-      minify: false,
-      lib: {
-        entry: entry,
-        formats: ["es"],
-        fileName: (format, entryName) => `${entryName}.${format}.js`,
-      },
-      rollupOptions: {
-        external: ["react", "react/jsx-runtime"],
-        output: {
-          dir: "../../exposure-burying/dist",
-          sourcemap: true,
-        },
-        plugins: [react()],
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    // 如下是默认值
+    // target: "modules",
+    minify: false,
+    lib: {
+      entry: entry,
+      formats: ["es"],
+      fileName: (format, entryName) => `${entryName}.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+      output: {
+        dir: "./exposure-burying/dist",
+        sourcemap: true,
       },
     },
-  };
+  },
 });
