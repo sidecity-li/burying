303b7cf6e86ef2ede20047ab4538c51f3202e3f4
sidecity.li <sidecity.li@osl.com>
2024-12-19 17:52:01 +0800

    feat: 优化demo

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { FiberOption, listen } from "@burying/automatic-burying";
+import { setupListen } from "@burying/automatic-burying";
 
 createRoot(document.getElementById("root")!).render(<App />);
 
-listen({
+setupListen({
   projectKey:
     "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInByb2plY3ROYW1lIjoi5LqL5L6L6aG555uuMSIsImlhdCI6MTczMzQ3MDYxMH0.VLZBzOWciGD3Miuz41v0-2Pz6HFdh3hsdNUTPcAZCpk",
-  callback: (event: any) => {
+  callback: (event) => {
     alert(`
       上报的事件:
       ${JSON.stringify(event, null, 4)}`);
